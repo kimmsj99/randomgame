50e34b6534ff42b7cfef0192aaef37f92f21c6f2
MinJu Kim <kimminju_99@naver.com>
2016-04-12 20:10:46 +0900

    #10 add Animation

--- app/src/main/java/kr/hs/emirim/ksmj0211/muhanchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ksmj0211/muhanchicken/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
             intent.putExtra("name", name);
             intent.putExtra("age", 10);
             startActivity(intent);
-
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
         }catch(NullPointerException e){
             Toast.makeText(this, "이름을 입력해 주세요!", Toast.LENGTH_LONG).show();
         }catch(Exception e){
